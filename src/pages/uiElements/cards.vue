29bf77749662d1368b5d9ec5812312b1df4e91a9
小玉拓 <taku_kodama@012grp.co.jp>
2021-12-25 15:30:45 +0900

    add card page

--- src/pages/uiElements/cards.vue
+++ src/pages/uiElements/cards.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="pageWrap">
     <h2>Card Components</h2>
+
     <card>
       <h3>DEFAULT</h3>
       <p>The unique stripes of zebras make them one of the animals most familiar to people. They occur in a variety of habitats, such as grasslands, savannas, woodlands, thorny scrublands, mountains, and coastal hills. Various anthropogenic factors have had a severe impact on zebra populations, in particular hunting for skins and habitat destruction. Grévy's zebra and the mountain zebra are endangered. While plains zebras are much more plentiful, one subspecies, the quagga.
-</p>
+      </p>
     </card>
+
   </div>
 </template>
 <script lang="ts">
